619c4294e2ba6d4da49f8b3e9d1e1b7471f496ec
Shuo Shen <sshen2000@yahoo.com>
2018-07-25 11:40:02 -0700

    Sleep 10s, batch and take 0.6 value to submit

--- Kiosk/Views/StorageServiceHelpers.cs
+++ Kiosk/Views/StorageServiceHelpers.cs
@@ -34,16 +34,80 @@
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace IntelligentKioskSample.Views
 {
     internal class StorageServiceHelpers
     {
         private CloudTableClient _tableClient;
+        private CloudTable _table;
+        private Dictionary<string, List<CustomerEntity>> _map = new Dictionary<string, List<CustomerEntity>>();
+        private Object _thisLock = new Object();
 
         public StorageServiceHelpers()
         {
+            Task.Run(DoWork);
+        }
+        public async Task DoWork()
+        {
+            Debug.WriteLine("RCCL: Start background thread");
+
+            while (true)
+            {
+                Dictionary<string, List<CustomerEntity>> processingMap = new Dictionary<string, List<CustomerEntity>>();
+                
+                lock(_thisLock)
+                {
+                    foreach (var item in _map.Keys)
+                    {
+                        processingMap.Add(item, _map[item]);
+                    }
+
+                    _map = new Dictionary<string, List<CustomerEntity>>();
+                }
+
+                foreach (var emotions in processingMap.Values)
+                {
+                    //foreach (var entry in emotions)
+                    {
+                        var selectedEntry = SelectAnEntry(emotions);
+                        // Create the TableOperation object that inserts the customer entity.
+                        TableOperation insertOperation = TableOperation.Insert(selectedEntry);
+
+                        Debug.WriteLine("RCCL: customerId:{0}, location:{1}, emostionScore: {2}", selectedEntry.PartitionKey, selectedEntry.Location, selectedEntry.EmotionScore);
+                        // Execute the insert operation.
+                        await _table.ExecuteAsync(insertOperation);
+                    }
+                }
+
+                // Batch wait interval 10 seconds
+                Task.Delay(10000).Wait();
+            }
+        }
+
+        private CustomerEntity SelectAnEntry(List<CustomerEntity> emotions)
+        {
+            emotions.Sort((emotion1, emotion2) => (emotion1.EmotionScore > emotion2.EmotionScore) ? -1 : 1);
+            Debug.WriteLine("RCCL: --->");
+            for (int i = 0; i < emotions.Count; i++) // (var emotion in emotions)
+            {
+                Debug.WriteLine("RCCL: {0}", emotions[i].EmotionScore);
+            }
+            Debug.WriteLine("RCCL: <---");
+
+            float floatIndex = emotions.Count * 0.4f;
+            int intIndex = (int)floatIndex;
+            if (intIndex > 0)
+            {
+                intIndex -= 1;
+            }
+
+            return emotions[intIndex];
         }
 
         public class CustomerEntity : TableEntity
@@ -68,23 +132,28 @@ namespace IntelligentKioskSample.Views
             {
                 CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=storage2018hack;AccountKey=zdPGuU+vEPjYlu/k6ddJQFAvwQWuSxckbWCAHaKKWCtjw7c+Puy03aBZ0iCl1S62/78rhBehmb7Vk213UtQAWg==;EndpointSuffix=core.windows.net");
                 _tableClient = storageAccount.CreateCloudTableClient();
+                // Create the CloudTable object that represents the "people" table.
+                _table = _tableClient.GetTableReference("videoResults");
             }
-
-            // Create the CloudTable object that represents the "people" table.
-            CloudTable table = _tableClient.GetTableReference("videoResults");
 
             // Create a new customer entity.
             CustomerEntity customer1 = new CustomerEntity(customerId);
             customer1.Location = location;
             customer1.EmotionScore = emotionScore;
 
-            // Create the TableOperation object that inserts the customer entity.
-            TableOperation insertOperation = TableOperation.Insert(customer1);
-
-            Debug.WriteLine("RCCL: customerId:{0}, location:{1}, emostionScore: {2}", customerId, location, emotionScore);
-            // Execute the insert operation.
-            await table.ExecuteAsync(insertOperation);
+            lock(_thisLock)
+            {
+                if (_map.ContainsKey(customerId))
+                {
+                    _map[customerId].Add(customer1);
+                }
+                else
+                {
+                    List<CustomerEntity> entries = new List<CustomerEntity>();
+                    entries.Add(customer1);
+                    _map[customerId] = entries;
+                }
+            }
         }
-
     }
 }
